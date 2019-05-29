92523eff9f81578074c1e24256af316fa30ac4c4
Scato Eggen <scato.eggen@gmail.com>
2019-05-29 14:20:17 +0200

    Fix bugs

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -3,6 +3,8 @@ import sys
 
 from airflow import DAG
 import airflow.utils.dates
+from airflow.contrib.operators.dataproc_operator \
+    import DataprocClusterCreateOperator, DataProcPySparkOperator, DataprocClusterDeleteOperator
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # NOQA
@@ -17,6 +19,8 @@ dag = DAG(
     dag_id='exercise4',
     default_args=args,
 )
+
+project_id = 'airflowbolcom-may2829-ba473316'
 
 with dag:
     psql_to_gcs = PostgresToGoogleCloudStorageOperator(
@@ -33,7 +37,14 @@ with dag:
     http_to_gcs = HttpToGoogleCloudStorageOperator(
         endpoint='/airflow-training-transform-valutas?date={{ ds }}&to=EUR',
         bucket='europe-west1-training-airfl-097953ee-bucket',
-        filename='data/currencies/ds={{ ds }}/properties_{}.json',
+        filename='data/currencies/ds={{ ds }}/properties_0.json',
         http_conn_id='http_gdd',
         task_id='http_to_gcs',
+    )
+
+    dataproc_create_cluster = DataprocClusterCreateOperator(
+        cluster_name='analyse-pricing-{{ ds }}',
+        project_id=project_id,
+        num_workers=2,
+        zone='europe-west4-a'
     )
