b941aa9bb69c07747ea7a9a3c052e5ec88240fa6
Scato Eggen <scato.eggen@gmail.com>
2019-05-29 13:45:40 +0200

    Add http_to_gcs to DAG

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -2,6 +2,8 @@ from airflow import DAG
 import airflow.utils.dates
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
+from dags.operators.http_to_gcs import HttpToGoogleCloudStorageOperator
+
 args = {
     'start_date': airflow.utils.dates.days_ago(14),
 }
@@ -18,7 +20,15 @@ with dag:
             """,
         parameters={'ds': '{{ ds }}'},
         bucket='europe-west1-training-airfl-097953ee-bucket',
-        filename='data/{{ ds }}/properties_{}.json',
+        filename='data/properties/ds={{ ds }}/properties_{}.json',
         postgres_conn_id='postgres_gdd',
         task_id='extract_land_registry_price_paid_uk',
+    )
+
+    http_to_gcs = HttpToGoogleCloudStorageOperator(
+        endpoint='/airflow-training-transform-valutas?date={{ ds }}&to=EUR',
+        bucket='europe-west1-training-airfl-097953ee-bucket',
+        filename='data/currencies/ds={{ ds }}/properties_{}.json',
+        http_conn_id='http_gdd',
+        task_id='http_to_gcs',
     )
